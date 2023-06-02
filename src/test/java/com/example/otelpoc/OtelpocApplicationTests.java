95c38969f48e58d1478866e9eab202bded0fb903
Felipe Kataoka Teshirogi <t719845@f1rst.com.br>
2023-06-02 15:22:30 -0300

    hello world spring

---
+++ src/test/java/com/example/otelpoc/OtelpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otelpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
